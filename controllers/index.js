1f0b209f2191ebcadc5e37f240db9e09a70b9647
mannygarcia98 <mannysg88@gmail.com>
2022-05-17 19:03:19 -0500

    resolve merge conflicts

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-<<<<<<< HEAD
-const apiRoutes = require('./api');
-const dashboardRoutes = require('./dashboard-routes.js');
+const dashboardRoutes = require("./dashboard");
+const apiRoutes = require("./api");
+const testRoutes = require("./test-routes");
 
-router.use('/', dashboardRoutes);
-router.use('/api', apiRoutes);
-=======
-const dashboardRoutes = require('./dashboard');
-const apiRoutes = require('./api');
-const testRoutes = require('./test-routes');
-
-router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
-router.use('/', testRoutes)
->>>>>>> main
+router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
+router.use("/", testRoutes);
 
 module.exports = router;
