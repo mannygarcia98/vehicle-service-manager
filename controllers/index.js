8540eaf5ff7379fc74effaf41b1cd6fea83e4ba6
Jermaine Walters <walte1jj@gmail.com>
2022-05-19 19:51:17 -0400

    controllers index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-const dashboardRoutes = require('./dashboard-routes');
 const apiRoutes = require('./api');
-const testRoutes = require('./test-routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/dashboard', dashboardRoutes);
+router.use('/', dashboardRoutes);
 router.use('/api', apiRoutes);
-router.use('/', testRoutes)
 
 module.exports = router;
