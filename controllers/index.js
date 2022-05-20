bd4ca6f363aea179120e3533bde59418e957412b
Mitchell Frey <mfcodingbc@gmail.com>
2022-05-19 21:58:54 -0400

    added javascript routes to dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,11 @@ const router = require("express").Router();
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
-module.exports = router;
+router.use((req, res) => {
+  res.status(404).end();
+});
 
+module.exports = router;
