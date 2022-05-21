960a6fe4b90ca10d6cafe500a64c85eba4a273d0
mannygarcia98 <mannysg88@gmail.com>
2022-05-20 19:53:34 -0500

    resolve merge conflicts

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -1,27 +1,27 @@
-const router = require('express').Router();
-const res = require('express/lib/response');
-const { Owner } = require('../../models');
-const withAuth = require('../../utils/auth');
-const passport = require('passport');
+const router = require("express").Router();
+const res = require("express/lib/response");
+const { Owner } = require("../../models");
+const withAuth = require("../../utils/auth");
+const passport = require("passport");
 
-router.post('/signup', async (req, res) => {
-    try {
-        const dbOwnerData = await Owner.create({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: req.body.password,
-        });
+router.post("/signup", async (req, res) => {
+  try {
+    const dbOwnerData = await Owner.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-        req.session.save(() => {
-            req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.loggedIn = true;
 
-            res.status(200).json(dbOwnerData);
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+      res.status(200).json(dbOwnerData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // router.post('/logins', async (req, res) => {
@@ -48,7 +48,7 @@ router.post('/signup', async (req, res) => {
 //         function(req, res) {
 //             res.redirect('/');
 //         };
-     
+
 //         req.session.save(() => {
 //             req.session.loggedIn = true;
 
@@ -60,78 +60,101 @@ router.post('/signup', async (req, res) => {
 //     }
 // });
 
-router.post('/logins', (req, res) => {
-    Owner.findOne({
-        where: {
-            email: req.body.email,
-        }
-    }).then(dbOwnerData => {
-        if (!dbOwnerData) {
-            res.status(400).json({ message: 'No owner with that email exists. Please try again.' });
-            return;
-        }
-
-        const validPassword = dbOwnerData.checkPassword(req.body.password);
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password. Please try again.' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.user_id = dbOwnerData.id;
-            req.session.username = dbOwnerData.username;
-            req.session.loggedIn = true;
-
-            res.status(200).json({ user: dbOwnerData, message:'You are logged in!'});
-        });
-
-        // passport.authenticate('local', 
-        // (err, user, info) => {
-        //     if (err) {
-        //         return (err);
-        //     }
-        
-        //     if (!user) {
-        //         return res.redirect('/logins?info=' + info);
-        //     }
-
-        //     req.logIn(user, function(err) {
-        //         if (err) {
-        //             return (err);
-        //         }
-
-        //         return res.redirect('/dashboard');
-        //     });
-
-        // })(req, res, next);
-        res.redirect('/dashboard/');
-    });
-});
-
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        res.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
+router.post("/logins", (req, res) => {
+  Owner.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbOwnerData) => {
+    if (!dbOwnerData) {
+      res.status(400).json({ message: "No owner with that email exists. Please try again." });
+      return;
     }
+
+    const validPassword = dbOwnerData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password. Please try again." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbOwnerData.id;
+      req.session.username = dbOwnerData.username;
+      req.session.loggedIn = true;
+
+      res.status(200).json({ user: dbOwnerData, message: "You are logged in!" });
+    });
+
+    // passport.authenticate('local',
+    // (err, user, info) => {
+    //     if (err) {
+    //         return (err);
+    //     }
+
+    //     if (!user) {
+    //         return res.redirect('/logins?info=' + info);
+    //     }
+
+    //     req.logIn(user, function(err) {
+    //         if (err) {
+    //             return (err);
+    //         }
+
+    //         return res.redirect('/dashboard');
+    //     });
+
+    // })(req, res, next);
+    res.redirect("/dashboard/");
+  });
+
+  // passport.authenticate('local',
+  // (err, user, info) => {
+  //     if (err) {
+  //         return next(err);
+  //     }
+
+  //     if (!user) {
+  //         return res.redirect('/logins?info=' + info);
+  //     }
+
+  //     req.logIn(user, function(err) {
+  //         if (err) {
+  //             return next(err);
+  //         }
+
+  //         return res.redirect('/dashboard');
+  //     });
+
+  // })(req, res, next);
+  // res.redirect('/dashboard/owner/:id');
+  // return;
+});
+// });
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    res.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
-router.get('/logins', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/dashboard/');
-        return;
-      }
-    res.render('login');
+router.get("/logins", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard/");
+    return;
+  }
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/dashboard/owner/:id');
-        return;
-      }
-    res.render('signup');
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard/owner/:id");
+    return;
+  }
+  res.render("signup");
 });
 
 module.exports = router;
